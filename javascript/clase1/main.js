61e2cf434977255f0b39fa04e1dc1cfebd13e8fc
neberpa <nestorebernalpardo@gmail.com>
2023-06-06 13:05:28 -0700

    creacion archivo js

--- javascript/clase1/main.js
+++ javascript/clase1/main.js
@@ -1,2 +1,11 @@
-let a=numero;
-let b=letra;
+let num1=10;
+            let num2= parseInt("5");
+            let num3=15;
+            let resultado= (num1*num2);
+
+
+
+        
+
+
+            document.write(typeof(num2));
