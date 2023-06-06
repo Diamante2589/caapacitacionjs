5da075e459b0ac9f201f3f5788eb20923a800eda
neberpa <nestorebernalpardo@gmail.com>
2023-06-06 15:03:24 -0700

    condicional if

--- javascript/clase1/main.js
+++ javascript/clase1/main.js
@@ -11,6 +11,31 @@
 
         condicional if
    */
+/*else if*/
+        let edad=prompt("introduce tu edad");
+        let valor=prompt("introduce el valor ahorrado");
+
+        if (edad >=21  && valor>=150){
+            document.write("la persona es mayor de edad   <br>");
+            document.write("puedes obtener tu carnet  tiene el dinero para el carnet<br>");
+        }
+
+        else{
+
+            document.write("el candidato no tiene la edad <br>");
+
+            let edadCandidato=18-edad;
+           
+            document.write("vuelve dentro de " + edadCandidato +  "años");
+        } 
+        
+        
+           document.write("aun tienes edad <br>");
+            document.write("<br><br>");
+            document.write("acabamos de evaluar la edad del candidato");
+
+    
+   
 
 
      
