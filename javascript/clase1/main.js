a00f921cf0baf88c7f651aa5253a36e723d71074
neberpa <nestorebernalpardo@gmail.com>
2023-06-10 07:29:28 -0700

    clase javawhile

--- javascript/clase1/main.js
+++ javascript/clase1/main.js
@@ -12,6 +12,7 @@
         condicional if
    */
 /*else if*/
+/*
         let edad=prompt("introduce tu edad");
         let valor=prompt("introduce el valor ahorrado");
 
@@ -20,10 +21,9 @@
             document.write("puedes obtener tu carnet  tiene el dinero para el carnet<br>");
         }
 
-        else{
+        else {
 
             document.write("el candidato no tiene la edad <br>");
-
             let edadCandidato=18-edad;
            
             document.write("vuelve dentro de " + edadCandidato +  "años");
@@ -34,8 +34,58 @@
             document.write("<br><br>");
             document.write("acabamos de evaluar la edad del candidato");
 
-    
+    */
    
+//ejercicio de switch
+/*
+    const weather=parseInt(prompt("ingrese codigo del 1 al 5 para informarle el clima"));
 
 
-     
+        switch(weather){
+            case(1):
+                console.log(`El dia es soleado`);
+                break;
+            case(2):
+                console.log(`Parcialmente nublado`);
+                break;
+            case(3):
+                console.log(`Mayormente nublado`); 
+                break;
+            case(4):
+                console.log(`Lluvioso`);
+                break;
+            case(5):
+                console.log(`Nieve`); 
+                break;
+            case(6):
+                console.log(`Tormenta`);  
+                break;                     
+
+            }
+
+
+
+            console.log(`Se termino el pronostico`);
+
+        */
+
+      //bucle while 01
+       //no sirve declarar la variable como constante, es necesario hacerlo como let
+       /*
+       let number=1;
+
+        while(number<10){
+            document.write(`imprimiendo en forma No ${number} <br>`);
+            document.write(`__________${number}___________________<br>`);
+            number++;
+        }
+        */
+        /*
+        let number=prompt("introduce un numero");
+
+        while(number<0){
+            number=prompt(`introduce un NUMERO POSITIVO`);
+        }
+    
+            document.write(Math.sqrt(number));
+            */
