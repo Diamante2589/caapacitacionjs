f2d5c9584fef5139730919af1bcae81b65d8e757
neberpa <nestorebernalpardo@gmail.com>
2023-06-20 08:15:50 -0700

    crear objeto carrito

--- javascript/15-PROYECTO-Carrito/js/app.js
+++ javascript/15-PROYECTO-Carrito/js/app.js
@@ -16,10 +16,27 @@ function cargarEventListeners(){
 function agregarCurso(e){
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')){
-        console.log(e.target);
+        const cursoSeleccionado=e.target.parentElement.parentElement;
+        leerDatos(cursoSeleccionado);
     }
    
 }
 
-//HAY QUE HACER TRAVERSING, LEE LA INOF QUE LE DIMOS CLICK Y EXTRAE EL CONTENIDO
+//HAY QUE HACER TRAVERSING, LEE LA INF QUE LE DIMOS CLICK Y EXTRAE LA INFORMACION DEL CURSO  C
 
+
+function leerDatos(curso){
+   console.log(curso);
+
+    //CREA UN CONTENIDO CON EL CONTENIDO DEL CURSO ACTUAL
+
+    const infoCurso={
+        imagen: curso.querySelector('img').src,
+        titulo:curso.querySelector('h4').textContent,
+        precio:curso.querySelector('span.u-pull-right').textContent
+        //cada curso tiene un id, asi accedemos a esos atributos
+        
+    }
+
+        console.log(infoCurso);
+}
