eea174da6bbd30a2a94b486b5ec2ac6bd49e0e73
neberpa <nestorebernalpardo@gmail.com>
2023-06-20 13:59:23 -0700

    agregar carrito

--- javascript/15-PROYECTO-Carrito/js/app.js
+++ javascript/15-PROYECTO-Carrito/js/app.js
@@ -3,6 +3,8 @@ const carrito= document.querySelector('#carrito');
 const contenedorCarrito= document.querySelector('#lista-carrito tbody');
 const listaCursos=document.querySelector('#lista-cursos');
 const vaciarCarritoBtn=document.querySelector('#vaciar-carrito')
+// despues de infocurso, creo esta variable
+let articulosCarrito=[];
 
 //1 CREO LAS FUNCIONES PARA  HACER LOS EVENT LISTENER
 
@@ -33,10 +35,23 @@ function leerDatos(curso){
     const infoCurso={
         imagen: curso.querySelector('img').src,
         titulo:curso.querySelector('h4').textContent,
-        precio:curso.querySelector('span.u-pull-right').textContent
+        precio:curso.querySelector('span.u-pull-right').textContent,
         //cada curso tiene un id, asi accedemos a esos atributos
-        
+        id:curso.querySelector('a').getAttribute('data-id'),
+        cantidad : 1
     }
+// una vez que  selleciono un curos leo  sus  datos  , voy a llenar el arreglo con esos objetos (let articulosCarrito)
+//agrega elementos al arreglo de carrito
+ // los (...)spreadoperator es para hacer copia
+        articulosCarrito=[...articulosCarrito,infoCurso] ;
+        console.log(articulosCarrito);
 
-        console.log(infoCurso);
+        //muestra el carrito en el html
+
+        function carritoHTML(){
+            articulosCarrito.forEach( curso => {
+                const row=document.createElement('tr')
+                //vamos a utilizar un html
+            })
+        }
 }
