5c864ff21ac863214980b0bae5814131b8e59d7c
neberpa <nestorebernalpardo@gmail.com>
2023-06-21 13:12:58 -0700

    trabajando en paso 6

--- javascript/15-PROYECTO-Carrito/js/app.js
+++ javascript/15-PROYECTO-Carrito/js/app.js
@@ -28,7 +28,7 @@ function agregarCurso(e){
 
 
 function leerDatos(curso){
-   console.log(curso);
+  // console.log(curso);
 
     //CREA UN CONTENIDO CON EL CONTENIDO DEL CURSO ACTUAL
 
@@ -40,10 +40,35 @@ function leerDatos(curso){
         id:curso.querySelector('a').getAttribute('data-id'),
         cantidad : 1
     }
+
+    //actualizar las cantidades paso (6)
+    //revisa si un elemento ya existe  
+    // (some)permite sobre un arreglo de objetos y verificar si elemento existe o no
+    const existe=articulosCarrito.some(curso => curso.id===infoCurso.id)
+        //console.log(existe);
+    if(existe){
+        //actualizamos la cantidad
+        const cursos=articulosCarrito.map(curso=>{//iteramos con este map, va a llegar el momento, en que vamos a encontrar
+            //el duplicado(if(curso.id etc))y lo unico que hacemos es incrementar el valor en uno, y lo retornamos
+            if(curso.id=== infoCurso.id){
+                curso.cantidad++
+                return curso;//retorna el objeto actualizado
+            }else{
+                return curso;//retorna los objetos que no son lo duplicados
+            }
+        });
+        articulosCarrito=[...cursos];
+        
+    }else{
+        articulosCarrito=[...articulosCarrito,infoCurso] ;
+        //agregamos el curso al carrito
+    }
+
+//hasta aca es el video 6
 // una vez que  selleciono un curos leo  sus  datos  , voy a llenar el arreglo con esos objetos (let articulosCarrito)
 //agrega elementos al arreglo de carrito
  // los (...)spreadoperator es para hacer copia
-        articulosCarrito=[...articulosCarrito,infoCurso] ;
+        
         console.log(articulosCarrito);
         carritoHTML();
        
@@ -57,14 +82,24 @@ function leerDatos(curso){
             limpiarHTML()//con esta istruccion primero limpiamos, para seguir añadiendo
             //recorre el carrito y genera el html
             articulosCarrito.forEach( curso => {
+                const{imagen, precio, titulo, cantidad, id}=curso;//reemplaza el colocar curso.imagen etc(5)destruction
+                console.log(curso);
                 const row=document.createElement('tr')
                 //vamos a utilizar un html
+                // con el td imagen se inyecta la imagen
                 row.innerHTML=`
+               
                 <td>
-                    ${curso.titulo}
+                    <img src= "${imagen}" width= "100">
                 </td>
+                <td>${titulo}</td>
+                <td>${precio}</td>
+                <td>${cantidad}</td>
+                <td><a href="#" class="borrar-curso" data-id=" ${id}"> X <td>
                 `;//estamos creando  el html, pero no lo hemos agregado a que se muestre, si recuerdas creamos un contenedor carrito y este contiene 
                 //tbody
+
+                //el td imagen se agrega con 120 mostrando el resto de la info
                 
                 //Agrega el HTML del carrito en el tbody
                 contenedorCarrito.appendChild(row);//vamos a agregar cada row en cada iteracion
